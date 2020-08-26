6681304f3562c38d07d6794af046c87cb567d4c8
benjivrik <31964614+benjivrik@users.noreply.github.com>
2020-08-26 00:45:33 -0400

    Working on Day28

--- Day27.java
+++ Day27.java
@@ -4,8 +4,8 @@
 // Community website : www.stuntbusiness.com
 // 
 // 30 Days - Q&A Java basic
-// Day 26 : GUI for collecting User information
-// Day 26 | IG : https://www.instagram.com/benjivrik/
+// Day 27 : GUI for collecting User information
+// Day 27 | IG : https://www.instagram.com/benjivrik/
 // ----------------------------------------------------
 // what would be the output of this program ?
 
